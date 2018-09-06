eee4b8c448c097436a1ccf340d240d7455dae9e9
amelgmar <amel@sotcatqlyst.com>
2018-09-06 15:29:24 +0100

    Added view and template for detailed blog post as well as CSS for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/default/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/default')
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 DATABASES = {
     'default': {
